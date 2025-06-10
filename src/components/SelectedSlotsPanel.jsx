b534794b4ef0dd4cbba191e9b2e3dd36c3d5ac7e
Ilya Breitburg <ilya.breytburg@gmail.com>
2025-06-10 21:41:13 +0200

    Fixed "Copied" bubble

--- src/components/SelectedSlotsPanel.jsx
+++ src/components/SelectedSlotsPanel.jsx
@@ -51,8 +51,8 @@ const SelectedSlotsPanel = ({
                   <Link size={14} />
                 </button>
                 {copiedRangeIndex === index && (
-                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 px-2 py-1 bg-neutral-700 text-white text-xs rounded whitespace-nowrap">
-                    Copied!
+                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-neutral-700 text-white text-xs rounded whitespace-nowrap">
+                    Copied
                     <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-2 border-r-2 border-t-2 border-l-transparent border-r-transparent border-t-neutral-700"></div>
                   </div>
                 )}
